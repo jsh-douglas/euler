196c28eb8a881f62dd4dd0d7f42939d9a96b40af
Josh Douglas <josh.douglas0108@gmail.com>
2020-04-20 00:58:05 +0100

    modified:   problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,9 @@
+#
+# Project Euler Problem 1 Solution
+# 
+# Author: Josh Douglas
+#
+# https://github.com/jsh-douglas/euler
+# https://projecteuler.net/problem=1
+
 print(sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0]))
