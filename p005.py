09334dde8360be102a47c1ee11d867a8a698dc6f
Josh Douglas <josh.douglas0108@gmail.com>
2020-04-21 23:08:57 +0100

    modified:   p005.py

--- p005.py
+++ p005.py
@@ -5,3 +5,17 @@
 # https://github.com/jsh-douglas/euler
 # https://projecteuler.net/problem=5
 
+nums = [11, 13, 16, 17, 19]
+found = False
+i = 2520
+while found == False:
+    suitable = True
+    for num in nums:
+        if i % num != 0:
+            suitable = False
+            break
+    if suitable:
+        print(i)
+        break
+    else:
+        i += 2520
